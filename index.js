73597ef7bcfe9fb56808ade330b9d529689d3f3a
jaepani <dev_jhkim@kakao.com>
2021-01-13 16:19:38 +0900

    db type change

--- index.js
+++ index.js
@@ -5,9 +5,30 @@ const prisma = new PrismaClient();
 async function main() {
     // ... you will write your Prisma Client queries here
     const allPost = await prisma.post.findMany();
-    const allUser = await prisma.user.findMany();
-    console.log('allUser', allUser);
     console.log('allPost', allPost);
+    // const allUser = await prisma.user.findMany();
+    // console.log('allUser', allUser);
+
+    // const inserUser = await prisma.user.create({
+    //     data : {
+    //         user_id : 'jaepani',
+    //         user_pw : '123123',
+    //     }
+    // });
+    // console.log(inserUser);
+
+    const inserPost = await prisma.post.create({
+        data: {
+            post_title: "jaepani post",
+            post_content: "jaepani post content",
+            user: {
+                connect: { user_id: 'test' }
+            }
+        },
+
+    });
+
+    console.log(inserPost);
 }
 
 main()
